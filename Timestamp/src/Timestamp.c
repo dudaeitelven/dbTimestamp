58baa3a92c5be29f9a93154fd96c999061a00a26
dudaeitelven <dudaeitelven@gmail.com>
2018-11-17 00:29:04 -0200

    First Commit

---
+++ Timestamp/src/Timestamp.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Timestamp.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("Ola Mundo !");
+
+	return EXIT_SUCCESS;
+}
