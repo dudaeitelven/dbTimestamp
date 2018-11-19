b481232aef6e855c59b692d2540b13ad47889677
dudaeitelven <32472327+dudaeitelven@users.noreply.github.com>
2018-11-19 17:54:36 -0200

    Update Timestamp.c

--- Timestamp/src/Timestamp.c
+++ Timestamp/src/Timestamp.c
@@ -1,4 +1,4 @@
-/*
+ /*
  ============================================================================
  Name        : timestamp.c
  Author      : Eduardo Eitelven
@@ -14,14 +14,15 @@
 #include <locale.h>
 
 typedef struct stDado {
-	char dado[5];
+	int dado;
 	int r_ts;
 	int w_ts;
+	struct stDado *prox;
 } nDado;
 
 typedef struct stOperacao {
 	int operacao; // 1 - Leitura | 2 - Escrita
-	nDado *dado;
+	int dado;
 	struct stOperacao *prox;
 } nOperacao;
 
@@ -29,18 +30,28 @@ typedef struct stTransacao{
 	int identificador;
 	int start;
 	int commit;
-	nOperacao *InicioOperacao;
+	struct stOperacao *InicioOperacao;
 	struct stTransacao *prox;
 } nTransacao;
 
-nTransacao *inicioTransacao;
+typedef struct stHistoria{
+	int tsTranscao;
+	int operacao;
+	int dado;
+	struct stHistoria *prox;
+} nHistoria;
 
-// historia inicial(HI) e historia final(HF) ?
+
+nDado *inicioDado;
+nTransacao *inicioTransacao;
+nHistoria *inicioHistoria;
 
 nTransacao* FAlocaTransacao() {
 	nTransacao *Transacao = (nTransacao*) malloc(sizeof(nTransacao));
 	return Transacao;
 }
+
+int identificadorTransacao = 0; //variavel para controle do numero da transacao.
 
 void cadastrarTransacao(int identificadorTransacao,int startTransacao,int commitTransacao) {
 	nTransacao *pnovo;
@@ -75,9 +86,7 @@ nOperacao* FAlocaOperacao() {
 	return Operacao;
 }
 
-void cadastrarOperacao () {
-
-	int transacaoBusca = 0;
+void cadastrarOperacao (int transacaoBusca, int dados) {
 	int achouTransacao = 0;
 
 	nOperacao *pnovo;
@@ -90,53 +99,61 @@ void cadastrarOperacao () {
 		return;
 	}
 
+
 	if (auxTransacao == NULL) {
 		printf("Nenhuma Transacao cadastrada ! \n");
 		return;
 	}
 
-	printf("Informe a Transacao: \n");
-	scanf("%d",&transacaoBusca);
-	fflush(stdin);
 
 	printf("Informe a operacao que deseja adcionar:\n 1 - Leitura ou 2 - Escrita\n");
 	scanf("%d",&pnovo->operacao);
 	fflush(stdin);
 
-	//printf("Informe o dado que deseja adicionar:");
-	//gets(dado);
-	//strcpy(pnovo->dado->dado,"x");
-	//pnovo->dado->r_ts = 0;
-	//pnovo->dado->w_ts = 0;
+
+	pnovo->dado = dados;
 	pnovo->prox = NULL;
 
 	while (auxTransacao != NULL) {
 		if (transacaoBusca == auxTransacao->identificador){
+
 			if (auxTransacao->InicioOperacao == NULL) {
 				auxTransacao->InicioOperacao = pnovo;
+				achouTransacao = 1;
+				break;
 			} else {
 
 				nOperacao *auxOperacao = auxTransacao->InicioOperacao;
 
-				while(auxOperacao != NULL) {
+				while(auxOperacao->prox != NULL) {
+
 					auxOperacao = auxOperacao->prox;
 				}
+
 				auxOperacao->prox = pnovo;
+				achouTransacao = 1;
+				break;
 			}
+
 		} else {
 			auxTransacao = auxTransacao->prox;
 		}
-		printf("Operacao inserida com sucesso !\n");
-		achouTransacao = 1;
-		return;
 
 	}
 
-	if (achouTransacao == 0) {
+	if (achouTransacao == 1) {
+		printf("Operacao inserida com sucesso !\n");
+		return;
+	} else {
 		printf("Transacao nao localizada.\n");
 		return;
 	}
 
+}
+
+nHistoria* FAlocaHistoria() {
+	nHistoria *Historia = (nHistoria*) malloc(sizeof(nHistoria));
+	return Historia;
 }
 
 void mostrarTransacoes () {
@@ -149,29 +166,96 @@ void mostrarTransacoes () {
 		}
 
 	while (aux != NULL){
-		printf("Identificador: %d\n",aux->identificador);
+		printf("Transacao: %d\n",aux->identificador);
 		printf("Start: %d\n",aux->start);
 		printf("Commit: %d\n",aux->commit);
-		printf("Operacoes \n");
+		printf("Operacoes: \n");
 
 		nOperacao *auxOperacao = aux->InicioOperacao;
 
+		if (auxOperacao == NULL) {
+			printf("    Sem Transacoes Cadastradas.\n");
+		}
+
 		while(auxOperacao != NULL){
 
-			printf("   Operacao: %d \n",auxOperacao->operacao);
-			printf("   Dado: %s \n",auxOperacao->dado->dado);
-			printf("   RTS: %d \n",auxOperacao->dado->r_ts);
-			printf("   WTS: %d \n\n",auxOperacao->dado->w_ts);
+			switch(auxOperacao->operacao){
+			case 1:
+				printf("   Operacao: Leitura \n");
+				break;
+			case 2:
+				printf("   Operacao: Escrita \n");
+				break;
+			}
+
+
+			switch(auxOperacao->dado){
+			case 1:
+				printf("   Dado: X\n");
+				break;
+			case 2:
+				printf("   Dado: Y\n");
+				break;
+			case 3:
+				printf("   Dado: Z\n");
+				break;
+			case 4:
+				printf("   Dado: A\n");
+				break;
+			case 5:
+				printf("   Dado: B\n");
+				break;
+			}
 
 			auxOperacao = auxOperacao->prox;
 
 		}
 
-		printf("\n\n");
+		printf("\n");
 
 		aux = aux->prox;
 	}
 }
+
+nDado* FAlocaDado() {
+	nDado *Dado = (nDado*) malloc(sizeof(nDado));
+	return Dado;
+}
+
+// Insere na memoria 5 tipos de dados X,Y,Z,A,B.
+
+void insereDados(int cont) {
+		nDado *auxDado = inicioDado;
+		nDado *pnovo;
+
+		pnovo = FAlocaDado();
+		if (!pnovo) {
+			printf("\n Nao foi possivel alocar \n");
+			return;
+		}
+
+		pnovo->dado = cont;
+		pnovo->r_ts = 0;
+		pnovo->w_ts = 0;
+		pnovo->prox = NULL;
+
+		if (inicioDado == NULL) {
+			inicioDado = pnovo;
+		} else {
+			while (auxDado->prox != NULL) {
+				auxDado = auxDado->prox;
+			}
+			auxDado->prox = pnovo;
+		}
+}
+
+
+
+
+
+
+
+
 
 void menu() {
 
@@ -185,17 +269,30 @@ void menu() {
 
 }
 
-int identificadorTransacao = 0;
-
 void carregaMenu(int Escolha) {
-
+	int escolheDado;
+	int transacaoBusca = 0;
 
 	switch (Escolha) {
 		case 1: //Cadastrar Transacoes em ordem sequencial.
 			cadastrarTransacao(++identificadorTransacao,1,1);
 			break;
 		case 2: //Cadastrar Operacoes de uma Transacao.
-			cadastrarOperacao();
+
+			printf("Informe a Transacao: \n");
+			scanf("%d",&transacaoBusca);
+			fflush(stdin);
+
+			printf("Informe o dado que deseja adicionar:\n");
+			printf("1 - X\n");
+			printf("2 - Y\n");
+			printf("3 - Z\n");
+			printf("4 - A\n");
+			printf("5 - B\n");
+			scanf("%d",&escolheDado);
+			fflush(stdin);
+
+			cadastrarOperacao(transacaoBusca,escolheDado);
 			break;
 		case 3: //Gerar Historia Inicial
 
@@ -213,6 +310,12 @@ void carregaMenu(int Escolha) {
 int main(void) {
 	setbuf(stdout, NULL);
 	int Escolha = 0;
+	int cont = 1;
+
+	while (cont <=5){
+		insereDados(cont++);
+	}
+
 
 	do {
 		do {
